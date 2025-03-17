ffbce2353992d009878775662873109ef484c9c2
aguspiza <345075+aguspiza@users.noreply.github.com>
2025-03-17 23:39:57 +0100

    fixes for TTS international text

--- sources/text_to_speech.py
+++ sources/text_to_speech.py
@@ -30,7 +30,7 @@ class Speech():
             print(f"Voice index {voice_idx} not found. Defaulting to 0.")
             voice_idx = 0
 
-        self.pipeline = KPipeline(lang_code=self.lang_map[language])
+        self.pipeline = KPipeline(lang_code=self.lang_map[language], repo_id="hexgrad/Kokoro-82M")
         self.voice = self.voice_map[language][voice_idx]
         self.speed = 1.2
         self.language = language
@@ -58,7 +58,7 @@ class Speech():
         )
         for i, (gs, ps, audio) in enumerate(generator):
             audio_file = 'sample.wav'
-            display(Audio(data=audio, rate=24000, autoplay=i==0), display_id=False)
+            #display(Audio(data=audio, rate=24000, autoplay=i==0), display_id=False)
             sf.write(audio_file, audio, 24000) # save each audio file
             if platform.system().lower() == "windows":
                 import winsound
@@ -123,19 +123,18 @@ class Speech():
             str: The cleaned text with URLs replaced by domain names, code blocks removed, etc..
         """
         lines = sentence.split('\n')
-        filtered_lines = [line for line in lines if re.match(r'^\s*[¡¿a-zA-ZáéíóúñüÁÉÍÓÚÑÜ]', line)]
+        filtered_lines = [line for line in lines if re.match(r'^\s*[¡!¿a-zA-ZáéíóúñüÁÉÍÓÚÑÜ]', line)]
         sentence = '. '.join(filtered_lines)
         sentence = re.sub(r'`.*?`', '', sentence)
         sentence = re.sub(r'https?://(?:www\.)?([^\s/]+)(?:/[^\s.]*)?', self.replace_url, sentence)
         sentence = re.sub(r'\b[\w./\\-]+\b', self.extract_filename, sentence)
         sentence = re.sub(r'\b-\w+\b', '', sentence)
-        sentence = re.sub(r'[^a-zA-Z0-9:;.,!?_ -áéíóúñüÁÉÍÓÚÑÜ]+', ' ', sentence)
+        sentence = re.sub(r'[^a-zA-Z0-9:;.,!?_\' -áéíóúñüÁÉÍÓÚÑÜ]+', ' ', sentence)
         sentence = re.sub(r'\s+', ' ', sentence).strip()
         if self.language == "spanish":
             sentence = sentence.replace('.com', ' punto com')
         else:
             sentence = sentence.replace('.com', ' dot com')
-        print(sentence)
         return sentence
 
 if __name__ == "__main__":
